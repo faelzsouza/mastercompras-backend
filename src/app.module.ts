793d54fe86af94f9791ce9c530383cfbf396053c
Rafael Souza <faelzsouza@gmail.com>
2021-12-08 12:03:10 -0300

    feat(providers): add PrismaService in providers list

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LojaModule } from './loja/loja.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { PrismaService } from './prisma/prisma.service';
+import { ProdutoModule } from './produto/produto.module';
 
 @Module({
-  imports: [LojaModule, PrismaModule],
+  imports: [LojaModule, ProdutoModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
