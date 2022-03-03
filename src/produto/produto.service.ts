a3cf1d09ed5b5473689d3dfcb3a110e7a900f707
Rafael Souza <faelzsouza@gmail.com>
2022-03-03 18:13:56 -0300

    feat: set `produto` and `loja` creation and `produto` update as well

--- src/produto/produto.service.ts
+++ src/produto/produto.service.ts
@@ -1,22 +1,68 @@
 import { Injectable } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
+import { LojaService } from 'src/loja/loja.service';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateProdutoDto } from './dto/create-produto.dto';
 import { UpdateProdutoDto } from './dto/update-produto.dto';
 
 @Injectable()
 export class ProdutoService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private readonly lojaService: LojaService,
+  ) {}
 
-  create(createProdutoDto: CreateProdutoDto) {
-    const data: Prisma.ProdutoCreateManyInput[] = [
-      ...createProdutoDto.produtos.map(produto => ({
-        ...produto,
-        data_compra: new Date(produto.data_compra)
-      }))
-    ]
-    //console.log(data)
-    return this.prisma.produto.createMany({ data });
+  async create(createProdutoDto: CreateProdutoDto) {
+    // Adiciona a loja no db ou adiciona mais um na quantidade de nf
+    let loja_db = await this.prisma.loja.findUnique({
+      where: { nome: createProdutoDto.produtos[0].loja },
+    });
+    if (!loja_db) {
+      loja_db = await this.lojaService.create({
+        nome: createProdutoDto.produtos[0].loja,
+        qtd_nf: 1,
+      });
+    } else {
+      await this.lojaService.update(loja_db.id, {
+        qtd_nf: loja_db.qtd_nf + 1,
+      });
+    }
+
+    const data = await Promise.all(createProdutoDto.produtos.map(async (p) => {
+      const produto_db = await this.prisma.produto.findUnique({
+        where: { nome: p.nome },
+      });
+      if (!produto_db) {
+        const novoProduto: Prisma.ProdutoCreateManyInput = {
+          nome: p.nome,
+          data_compra: new Date(p.data),
+          loja_id: loja_db.id,
+          qtd_soma: 1,
+          preco: p.valor,
+          valor_max: p.valor,
+          valor_med: p.valor,
+          valor_min: p.valor,
+          valor_soma: p.valor,
+        };
+        return novoProduto;
+      } else {
+        const produto: Prisma.ProdutoUncheckedUpdateInput = {
+          ...produto_db,
+          data_compra: new Date(p.data),
+          loja_id: loja_db.id,
+          qtd_soma: produto_db.qtd_soma + 1,
+          preco: p.valor,
+          valor_max:
+            p.valor > produto_db.valor_max ? p.valor : produto_db.valor_max,
+          valor_min:
+            p.valor < produto_db.valor_min ? p.valor : produto_db.valor_min,
+          valor_soma: produto_db.valor_soma + p.valor,
+          valor_med: (produto_db.valor_soma + p.valor) / (produto_db.qtd_soma + 1),
+        };
+        this.update(produto_db.id, produto);
+      }
+    }, this));
+    return await this.prisma.produto.createMany({data})
   }
 
   findAll() {
@@ -27,8 +73,11 @@ export class ProdutoService {
     return `This action returns a #${id} produto`;
   }
 
-  update(id: number, updateProdutoDto: UpdateProdutoDto) {
-    return `This action updates a #${id} produto`;
+  async update(
+    id: number,
+    updateProdutoDto: Prisma.ProdutoUncheckedUpdateInput,
+  ) {
+    return await this.prisma.produto.update({where:{id}, data: updateProdutoDto});
   }
 
   remove(id: number) {
