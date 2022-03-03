c0d6174f66996b623db7a7dc4c5069c27b3d2668
Rafael Souza <faelzsouza@gmail.com>
2022-03-03 18:08:00 -0300

    feat(providers): add `LojaService` dependencie providers

--- src/produto/produto.module.ts
+++ src/produto/produto.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProdutoService } from './produto.service';
 import { ProdutoController } from './produto.controller';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { LojaService } from 'src/loja/loja.service';
 
 @Module({
   controllers: [ProdutoController],
-  providers: [ProdutoService, PrismaService],
+  providers: [ProdutoService, PrismaService, LojaService],
 })
 export class ProdutoModule {}
